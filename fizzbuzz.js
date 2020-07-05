a05f884fe88658dad25b8bd1e1156080ea41d753
taiga morikawa <taiga.morikawa@gmail.com>
2020-07-06 01:56:51 +0900

    improve on code review at 001

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,29 @@
 //fizzbuzz問題：倍数を出力する
 function fizzbuzz(){
 
-    var result = document.getElementById('res00001');
+    let result = document.getElementById('res00001');
 
     //テキストボックスの値取得
-    var fn = document.getElementById('fizznum').value;
-    var bn = document.getElementById('buzznum').value;
+    const fn = parseInt(document.getElementById('fizznum').value);
+    const bn = parseInt(document.getElementById('buzznum').value);
 
     //初期化
     while (result.firstChild) {
         result.removeChild(result.firstChild);
     }
 
+    //要件：文字列・空欄・小数の判定
+    isError = v => {
+        let jv = parseInt(v, 10);
+        if (isNaN(jv) || (jv != v)) {
+            return true;
+        }
+        return false;
+    };
+
     //エラーチェック
-    if (isError(fn) || isError(bn)){
-        var msg = document.createElement('p');
+    if (isError(fn) || isError(bn)) {
+        let msg = document.createElement('p');
         msg.className = "result";
         msg.innerText = "整数値を入力してください";
         result.appendChild(msg);
@@ -22,62 +31,45 @@ function fizzbuzz(){
     }
 
     //初期化
-    var fi = 1; var bi = 1;
-    var ul = document.createElement('ul');
+    let ul = document.createElement('ul');
 
-    //数値へ型変換明示
-    fn = parseInt(fn);
-    bn = parseInt(bn);
+    let fm = fn;
+    let fi = 1;
 
-    do{
+    let bm = bn;
+    let bi = 1;
+
+    //要件：倍数を計算して二桁まで表示する。
+    while((fm < 100) && (bm < 100)){
+
         //倍数計算処理
-        var fm = fn * fi;
-        var bm = bn * bi;
+        fm = fn * fi;
+        bm = bn * bi;
 
-        //要件：倍数を計算して二桁まで表示する。
-        if (fm >= 100 && bm >= 100){
-            
-            //fizzNum,buzzNum両方三桁以上の数字を入力された場合の対処
-            if (ul.hasChildNodes()) { 
-                result.appendChild(ul);
-            }
-            return;
-        }
-
-        var li = document.createElement('li');
+        let li = document.createElement('li');
         li.className = "result";
 
         //要件：小さい順表示
-        if(fm == bm){
-            li.innerText = "FizzBuzz" + " " + fm;
+        if (fm === bm) {
             //要件：公倍数の場合 : FizzBuzz出力
-            fi++; bi++;
-        }else if(fm < bm){
+            li.innerText = "FizzBuzz" + " " + fm;
+            fi++;
+            bi++;
+        } else if(fm < bm) {
             li.innerText = "Fizz" + " " + fm;
             fi++;
-        }else{
+        } else {
             li.innerText = "Buzz" + " " + bm;
             bi++;
         }
         
         ul.appendChild(li);
 
-    } while (true);
- 
-}
-
-//エラーチェック
-function isError(v) {
-
-    var jv = parseInt(v, 10);
-
-    if (isNaN(jv)){
-        //要件：文字列・空欄の場合
-        return true;
-    }else if(jv != v){
-        //要件：小数の場合
-        return true;
     }
 
-    return false;
+    //fizzNum,buzzNum両方三桁以上の数字を入力された場合の対処
+    if (ul.hasChildNodes()) {
+        result.appendChild(ul);
+    }
+ 
 }
