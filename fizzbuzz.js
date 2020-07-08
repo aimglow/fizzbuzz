61027ba0f715cef01dd57b278127aeca313302a2
taiga morikawa <taiga.morikawa@gmail.com>
2020-07-09 00:23:36 +0900

    improve by review at 005

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,11 @@
 //fizzbuzz問題：倍数を出力する
 const fizzbuzz = () => {
     
-    const result = document.getElementById('out-result');
+    const result = document.getElementById("out-result");
 
     //テキストボックスの値取得
-    const txtfnum = document.getElementById('fizznum').value;
-    const txtbnum = document.getElementById('buzznum').value;
+    const textFizzNum = document.getElementById("fizznum").value;
+    const textBuzzNum = document.getElementById("buzznum").value;
 
     //初期化
     while (result.firstChild) {
@@ -13,58 +13,58 @@ const fizzbuzz = () => {
     }
 
     //要件：文字列・空欄・小数の判定
-    const judgeInvalidValue = val => {
-        const intVal = parseInt(val, 10);
-        const floatVal = parseFloat(val);
-        return isNaN(intVal) || (floatVal !== intVal);
+    const judgeInvalidValue = value => {
+        const intValue = parseInt(value, 10);
+        const floatValue = parseFloat(value);
+        return isNaN(intValue) || (floatValue !== intValue);
     };
 
     //入力判定
-    if (judgeInvalidValue(txtfnum) || judgeInvalidValue(txtbnum)) {
-        const msg = document.createElement('p');
-        msg.className = "result";
-        msg.innerText = "整数値を入力してください";
-        result.appendChild(msg);
+    if (judgeInvalidValue(textFizzNum) || judgeInvalidValue(textBuzzNum)) {
+        const message = document.createElement("p");
+        message.className = "result";
+        message.innerText = "整数値を入力してください";
+        result.appendChild(message);
         return;
     }
 
     //初期化
-    const ul = document.createElement('ul');
+    const ul = document.createElement("ul");
     ul.className = "result";
-    const fn = parseInt(txtfnum);
-    const bn = parseInt(txtbnum);
+    const fizzNum = parseInt(textFizzNum);
+    const buzzNum = parseInt(textBuzzNum);
 
     let fi = 1;
-    let fm = fn * fi;
+    let fizzMultiple = fizzNum * fi;
 
     let bi = 1;
-    let bm = bn * bi;
+    let buzzMultiple = buzzNum * bi;
 
     //要件：倍数を計算して二桁まで表示する。
-    while((fm < 100) || (bm < 100)){
+    while((fizzMultiple < 100) || (buzzMultiple < 100)){
 
-        const li = document.createElement('li');
+        const li = document.createElement("li");
         li.className = "result";
 
         //要件：小さい順表示
-        if (fm === bm) {
+        if (fizzMultiple === buzzMultiple) {
             //要件：公倍数の場合 : FizzBuzz出力
-            li.innerText = "FizzBuzz" + " " + fm;
+            li.innerText = "FizzBuzz" + " " + fizzMultiple;
             fi++;
             bi++;
-        } else if(fm < bm) {
-            li.innerText = "Fizz" + " " + fm;
+        } else if(fizzMultiple < buzzMultiple) {
+            li.innerText = "Fizz" + " " + fizzMultiple;
             fi++;
         } else {
-            li.innerText = "Buzz" + " " + bm;
+            li.innerText = "Buzz" + " " + buzzMultiple;
             bi++;
         }
         
         ul.appendChild(li);
 
         //倍数計算処理
-        fm = fn * fi;
-        bm = bn * bi;
+        fizzMultiple = fizzNum * fi;
+        buzzMultiple = buzzNum * bi;
 
     }
 
